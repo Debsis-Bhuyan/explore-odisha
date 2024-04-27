be7536c8d63dd069a1b8acc2c2c3774c62722378
Debsis-Bhuyan <debasisbhuyan2@gmail.com>
2024-04-27 17:13:56 +0530

    api

--- src/utils/apiCall.jsx
+++ src/utils/apiCall.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const API_URL = "https://explore-odisha-backend.onrender.com";
-export const API_URL = "http://localhost:5001";
+export const API_URL = "https://explore-odisha-backend.onrender.com";
+// export const API_URL = "http://localhost:5001";
 
 export const getGoogleSignUp = async (accessToken) => {
   try {
